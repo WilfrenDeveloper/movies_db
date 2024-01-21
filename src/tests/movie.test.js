1a97ffacf58f7fd1ab936576b965753a6c97dfbc
WilfrenDeveloper <https://github.com/WilfrenDeveloper/e-commerce-whit-React.git>
2024-01-21 16:33:55 -0500

    advances

--- src/tests/movie.test.js
+++ src/tests/movie.test.js
@@ -1,5 +1,9 @@
 const request = require('supertest');
 const app = require('../app.js');
+const Actor = require('../models/Actor.js');
+const Director = require('../models/Director.js');
+const Genre = require('../models/Genre.js');
+require('../models')
 
 let id;
 
@@ -30,6 +34,44 @@ test('PUT /movies/:id debe actualizar una película', async () => {
     const res = await request(app).put(`/movies/${id}`).send(updateMovie);
     expect(res.status).toBe(200);
     expect(res.body.name).toBe(updateMovie.name);
+});
+
+test('POST /movies/:id/actors debe insertar todos los actores', async () => {
+    const actor = await Actor.create({
+        firstName: "Charlie",
+        lastName: "Hunnam",
+        nationality: "Reino Unido",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Charlie_Hunnam_at_Berlinale_2017.jpeg/250px-Charlie_Hunnam_at_Berlinale_2017.jpeg",
+        birthday: "04-10-1980",
+    });
+    const res = await request(app).post(`/movies/${id}/actors`).send([actor.id]);
+    await actor.destroy;
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(1);
+});
+
+test('POST /movies/:id/directors debe insertar todos los directores', async () => {
+    const director = await Director.create({
+        firstName: "Charlie",
+        lastName: "Hunnam",
+        nationality: "Reino Unido",
+        image: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/48/Charlie_Hunnam_at_Berlinale_2017.jpeg/250px-Charlie_Hunnam_at_Berlinale_2017.jpeg",
+        birthday: "04-10-1980",
+    });
+    const res = await request(app).post(`/movies/${id}/directors`).send([director.id]);
+    await director.destroy;
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(1);
+});
+
+test('POST /movie/:id/genres debe insertar todos los géneros', async () => {
+    const genre = await Genre.create({
+        name : "Comedia"
+    });
+    const res = await request(app).post(`/movies/${id}/genres`).send([genre.id]);
+    await genre.destroy();
+    expect(res.status).toBe(200);
+    expect(res.body.length).toBe(1);
 });
 
 test('DELETE /movies/:id debe eliminar la película', async () => {
